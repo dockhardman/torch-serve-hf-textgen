4bf5f990c77e45eb34b29b2fb9b760615016466e
Allen Chou <f1470891079@gmail.com>
2023-12-09 13:32:13 +0000

    Debug datatype error

--- handlers/hf_text_generation_handler.py
+++ handlers/hf_text_generation_handler.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict, List, Text, TypedDict, Union
 
 import torch
 import transformers
@@ -8,6 +9,14 @@ from ts.torch_handler.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+class RequestCall(TypedDict):
+    body: Union["RequestBody", List["RequestBody"]]
+
+
+class RequestBody(TypedDict):
+    text: Text
 
 
 class TransformersHandler(BaseHandler):
@@ -39,25 +48,36 @@ class TransformersHandler(BaseHandler):
 
         self.initialized = True
 
-    def preprocess(self, data):
+    def preprocess(self, data: List["RequestCall"]) -> List[Text]:
         """
         Preprocessing input request. This prepares the input text.
         """
-        print(f"Data: {data}")
-        texts = [request.get("data") or request.get("body") for request in data]
-        texts = [
-            text.decode("utf-8") if isinstance(text, (bytes, bytearray)) else text
-            for text in texts
-        ]
+        if not data:
+            return []
+        request_body = data[0].get("data") or data[0].get("body")
+        if isinstance(request_body, Dict):
+            request_body = [request_body]
+        texts: List[Text] = []
+        for request in request_body:
+            text = request.get("text")
+            if text is None:
+                continue
+            texts.append(
+                text.decode("utf-8") if isinstance(text, (bytes, bytearray)) else text
+            )
         print(f"Texts: {texts}")
         return texts
 
-    def inference(self, inputs):
+    def inference(self, inputs: List[Text]) -> List[Text]:
         """
         Generate text using the model pipeline.
         """
+        if not inputs:
+            return []
+        if isinstance(inputs, Text):
+            inputs = [inputs]
         print(f"Inputs: {inputs}")
-        outputs = self.pipeline(
+        results = self.pipeline(
             inputs,
             do_sample=True,
             num_return_sequences=1,
@@ -67,7 +87,10 @@ class TransformersHandler(BaseHandler):
             top_k=50,
             repetition_penalty=1.2,
         )
-        return [output["generated_text"] for output in outputs]
+        print(f"Results: {results}")
+        return [
+            "\n".join([res["generated_text"] for res in result]) for result in results
+        ]
 
     def postprocess(self, inference_output):
         """
