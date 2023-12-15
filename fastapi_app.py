1c364b19cf7b8b7f22b3178ca06942044f001c1b
Allen Chou <f1470891079@gmail.com>
2023-12-15 12:27:40 +0000

    Get model meta api

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,8 +1,8 @@
 import uuid
-from typing import Any, Dict, List, Text
+from typing import Any, Dict, List, Text, Union
 
 import aiohttp
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings
 from yarl import URL
@@ -15,14 +15,37 @@ class Settings(BaseSettings):
 
 
 class ModelsResponse(BaseModel):
-    models: List["ModelMeta"]
+    models: List[Union["ModelsMeta", "ModelsMetaBasic"]]
 
 
-class ModelMeta(BaseModel):
+class ModelsMetaBasic(BaseModel):
     modelName: Text
     modelUrl: Text
 
 
+class ModelsMeta(ModelsMetaBasic):
+    modelVersion: Text
+    runtime: Text
+    minWorkers: int
+    maxWorkers: int
+    batchSize: int
+    maxBatchDelay: int
+    loadedAtStartup: bool
+    workers: List["ModelsWorker"]
+    jobQueueStatus: Dict[Text, Any]
+
+
+class ModelsWorker(BaseModel):
+    id: Text
+    startTime: Text
+    status: Text
+    memoryUsage: int
+    pid: int
+    gpu: bool
+    gpuUsage: Text
+
+
+ModelsMeta.model_rebuild()
 ModelsResponse.model_rebuild()
 
 
@@ -58,13 +81,32 @@ def create_app():
                 resp.raise_for_status()
                 return await resp.json()
 
-    @app.get("/api/v1/llm/models", response_model=ModelsResponse)
+    @app.get("/api/v1/llm/models", response_model=List[ModelsResponse])
     async def llm_models():
         url = URL(app_settings.host_ts_management_service).with_path("models")
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 resp.raise_for_status()
                 return await resp.json()
+
+    @app.get("/api/v1/llm/models/{model_name}", response_model=List[ModelsMeta])
+    async def llm_model_info(model_name: Text):
+        url = URL(app_settings.host_ts_management_service).with_path(
+            f"models/{model_name}"
+        )
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                try:
+                    resp.raise_for_status()
+                    return await resp.json()
+                except aiohttp.ClientResponseError as e:
+                    if resp.status == 404:
+                        raise HTTPException(
+                            status_code=404, detail=f"Model '{model_name}' not found"
+                        )
+                    raise HTTPException(status_code=500, detail=str(e))
+                except Exception as e:
+                    raise HTTPException(status_code=500, detail=str(e))
 
     @app.post("/api/v1/llm/{model_name}/chat")
     async def llm_chat():
