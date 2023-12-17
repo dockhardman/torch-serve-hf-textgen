43717338547353234bd08a9cf16a4d94c1b961c7
Allen Chou <f1470891079@gmail.com>
2023-12-17 13:54:45 +0000

    Complete torchserve docker container

--- dockerfile.ts
+++ dockerfile.ts
@@ -2,19 +2,13 @@ FROM pytorch/torchserve:latest-gpu
 
 ENV MODEL_NAME=llama2-7b-chat
 
-# Install Poetry
-RUN curl -sSL https://install.python-poetry.org | POETRY_HOME=/opt/poetry python - && \
-    cd /usr/local/bin && \
-    ln -s /opt/poetry/bin/poetry && \
-    poetry config virtualenvs.create false
+# Copy model artifacts, custom handler and other dependencies
+COPY ./requirement.txt /home/model-server/
+COPY ./model_store/$MODEL_NAME.mar /home/model-server/model_store/$MODEL_NAME.mar
+COPY ./config/config.properties /home/model-server/config/config.properties
 
 # Install Dependencies
-WORKDIR /home/model-server/
-COPY ./pyproject.toml ./poetry.lock* /home/model-server/
-RUN poetry install -E ts --no-root && poetry show
-
-# Copy model artifacts, custom handler and other dependencies
-COPY ./model_store/llama2-7b-chat.mar /home/model-server/model_store/
+RUN pip install -r /home/model-server/requirement.txt
 
 EXPOSE 8085
 EXPOSE 8086
